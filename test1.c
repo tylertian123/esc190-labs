e6313371a3fa9ab9e92e5083b68b7da12d2fed13
Tyler Tian <tylertian123@gmail.com>
2022-01-25 00:10:27 -0500

    Additional test case

--- test1.c
+++ test1.c
@@ -4,10 +4,12 @@
 int main() {
 	double test_p1 = split_bill(50.01, 0.13, 0.15, 2);
 	printf("Split: $%.2f\n", test_p1);
-	double test_p2 = split_bill(50.01, 0, 0, 2);
-	printf("Split: $%.2f\n", test_p2);
-	double test_p3 = split_bill(50.00, 0, 0, 2);
-	printf("Split: $%.2f\n", test_p3);
+	test_p1 = split_bill(50.01, 0, 0, 2);
+	printf("Split: $%.2f\n", test_p1);
+	test_p1 = split_bill(50.00, 0, 0, 2);
+	printf("Split: $%.2f\n", test_p1);
+	test_p1 = split_bill(62.05, 0.15, 0.30, 4);
+	printf("Split: $%.2f\n", test_p1);
 
 	char test_food[] = "Three Cheese Pizza";
 	printf("Sandy would%s eat '%s'.\n", sandy_eats(test_food) ? "" : " NOT", test_food);
